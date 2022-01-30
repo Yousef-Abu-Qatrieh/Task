70f77938d115be12d30fbfa0bf38e4ff423ad154
Yousef-Abu-Qatrieh <yousef_1995q@hotmail.com>
2022-01-30 06:03:10 +0200

    update

---
+++ regex.test.js
@@ -0,0 +1,100 @@
+'use strict';
+
+
+
+/* Write a function that take a string and return true if the string only contain uppercase and lowercase
+characters (no numbers and symbols) and it should end with capital A else return false */
+
+function capitalA(s){
+  let regex=/[0-9]/g;
+  let regex1=/\w/gi;
+  let regex2=/(A)\b/g;
+  
+    if(regex.test(s)==true){
+        return false;
+    }if(regex1.test(s)==true&&regex2.test(s)==true){
+
+return true;
+
+    }
+    else
+    return false;
+}
+
+
+/* Write a function that take a string and return true if the the sting is following the emails pattern
+which end with io (example@example.io) */
+
+function ioEmail(email){
+
+    let regex3=/(.io)$\b/g;
+    let regex4=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+   if(regex3.test(email)==true&&regex4.test(email)==true){
+       return true
+   }
+    else
+    return false
+    
+}
+
+
+/* You have a text that contain image names with their extention you need to write a function to 
+find all images in that text and return their names and extention in an array 
+
+required extention are jpg, jpeg and png.
+
+*/
+
+function imagesSearcher(text){
+    let arr = [];
+    let newArr=[]
+    newArr.push(text)
+    
+   for(let i=0;i<newArr.length;i++){
+    
+    let regex5=/\/[^"']*\.(?:png|jpg|jpeg|gif|png|sv/g
+    text=newArr[i]
+
+       
+    if(regex5.test(text)==true){
+        arr.push(text)
+
+    }
+    else
+    continue
+
+    
+   }
+
+    
+    return arr
+}
+
+
+describe("Test capitalA", ()=>{
+    test("It should return true if the input has uppercase and lowercase characters (no numbers and symbols) and it should end with capital A else return false ", () => {
+        expect(capitalA("Hello world A")).toStrictEqual(true);
+
+        expect(capitalA("Hello world")).toStrictEqual(false);
+
+        expect(capitalA("Hello world a")).toStrictEqual(false);
+    })
+});
+
+describe("Test ioEmail", () => {
+    test("It should return true if the input is in email format that end with .io", () => {
+        expect(ioEmail("example@example.io")).toStrictEqual(true);
+        expect(ioEmail("ex@ample@example.io")).toStrictEqual(false);
+        expect(ioEmail("ex.ample@example.io")).toStrictEqual(true);// check this test case it was false and nothing wrong ;
+        expect(ioEmail("example@example.gmail")).toStrictEqual(false);
+    })
+});
+
+
+describe("Test imagesSearcher", () => {
+    test("It should return all images names that end with jpg, jpeg and png extention", () => {
+        expect(imagesSearcher("Lorem ipsum dolor sit amet, consectetur adipiscing elit, cat.png sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. dog.jpg Ut enim ad minim veniam, quis nostrud exercitation ullamco cow.jpeg laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")).toStrictEqual(['cat.png', 'dog.jpg', 'cow.jpeg']);
+        expect(imagesSearcher("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")).toStrictEqual([]);
+        expect(imagesSearcher("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. horse.gif Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore dolphin.pdf eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa mouse.tiff qui officia deserunt mollit anim id est laborum.")).toStrictEqual([]);
+    })
+})
